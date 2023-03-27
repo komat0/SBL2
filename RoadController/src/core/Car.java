d9862d1430ec97f9be2c59c830ca1be16d51a739
komat0 <111368527+komat0@users.noreply.github.com>
2023-03-27 11:34:06 +0700

    Lesson 3.7 В классе Car проекта RoadController создайте геттеры и сеттеры для всех переменных класса.

--- RoadController/src/core/Car.java
+++ RoadController/src/core/Car.java
@@ -15,4 +15,44 @@ public class Car {
             special + "Автомобиль с номером " + number +
             ":\n\tВысота: " + height + " мм\n\tМасса: " + weight + " кг" + "\n\t" + vehicle;
     }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHasVehicle(boolean hasVehicle) {
+        this.hasVehicle = hasVehicle;
+    }
+
+    public void setSpecial(boolean special) {
+        isSpecial = special;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public boolean isHasVehicle() {
+        return hasVehicle;
+    }
+
+    public boolean isSpecial() {
+        return isSpecial;
+    }
 }
